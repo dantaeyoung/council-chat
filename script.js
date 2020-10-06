ebbe1283ec6cb756d8f2e6e3ad0f4a2675284ddb
Glitch (hello-webpage) <none>
2020-10-06 19:50:03 +0000

    🗽🙎 Checkpoint ./index.html:171012/415 ./style.css:171012/127 ./script.js:171012/5872

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
 /* globals firebase */
 
-
 // Get a reference to the database service
 var database = firebase.database();
 
-
 function setMyname(name) {
-  database.ref("myname").set({
-    name: name,
+  database.ref("ourname").set({
+    name: name
   });
-  console.log("ok")
+}
+
+function addAge(number) {
+  var currentAge = parseInt($("#currentAge").text());
+  
+  if(currentAge == "") { currentAge = 0; }
+  
+  database.ref("ourage").set({
+    age: currentAge + number
+  });
+
+  
 }
 
 
-
-
 $(document).ready(function() {
-  
 
+  database.ref("ourname").on("value", function(snapshot) {
+    $("#currentName").text(snapshot.val().name);
+  });
+  
+  database.ref("ourage").on("value", function(snapshot) {
+    $("#currentAge").text(snapshot.val().age);
+  });
   
   
   $("#setName").click(function() {
-  
-      //alert("what");
-      setMyname($("#nameInput").val());
-    console.log("settingmyanme")
+    //alert("what");
+    setMyname($("#nameInput").val());
+    console.log("settingmyanme");
+  });
+
+  $("#addAge").click(function() {
+    addAge(2);
   })
 
-  
-  
-  database
-    .ref('myname')
-    .on('value', function(snapshot) {
-      $("#currentName").text(snapshot.val().name);
-    });
+  $("#subtractAge").click(function() {
+    addAge(-2);
+  })
 
-  
-})
-
-
-
+});
