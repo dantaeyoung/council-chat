3a8a548ac8c4740646a33ea675e0a36020e2cc74
Glitch (hello-webpage) <none>
2020-10-06 19:14:23 +0000

    🍓👄 Checkpoint ./index.html:171012/1987 ./script.js:171012/242

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+// Get a reference to the database service
+var database = firebase.database();
+
+
