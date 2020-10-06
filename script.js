034bc207ecc016793b2c53e900274590fde5fe0d
Glitch (hello-webpage) <none>
2020-10-06 19:26:04 +0000

    ⛵️👭 Checkpoint ./index.html:171012/562 ./script.js:171012/2218

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+/* globals firebase */
+
 
 // Get a reference to the database service
 var database = firebase.database();
+
+
+function setMyname(name) {
+  database.ref("myname").set({
+    name: name,
+  });
+}
+
+
+
+$(document).ready(function() {
+  
+  
+  $("#setName").click(function() {
+  
+      //alert("what");
+      setMyname("dan");
+  })
+
+  
+  
+  var starCountRef = firebase.database().ref('myname');
+  starCountRef.on('value', function(snapshot) {
+    $("#currentName").text("whoo");
+  });
+
+
+  
+})
+
 
 
